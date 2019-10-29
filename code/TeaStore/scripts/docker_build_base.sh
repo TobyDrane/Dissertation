9409c9c2dcade8220bbc130109c7e4deb6a934f8
Toby Drane <tobydrane@gmail.com>
2019-10-29 13:13:30 +0000

    monolithic finished

--- code/TeaStore/scripts/docker_build_base.sh
+++ code/TeaStore/scripts/docker_build_base.sh
@@ -2,10 +2,10 @@
 
 # Remove old Docker image
 docker rmi teastore:base
-docker image prune
+docker image prune -a
 
 # Lets build the base image
-docker build --no-cache=true -t teastore:base ./utilities/tools.descartes.teastore.dockerbase/
+docker build -t teastore:base ./utilities/tools.descartes.teastore.dockerbase/
 
 echo "Created TeaStore base image, printing all Docker images..."
 
--- code/TeaStore/scripts/start_monolithic.sh
+++ code/TeaStore/scripts/start_monolithic.sh
@@ -6,11 +6,15 @@
 docker run -p 3306:3306 -d --name teastore-db descartesresearch/teastore-db
 echo "TeaStore db running..."
 
+# Lets now create and run the RabbitMQ image - this is needed for Keiker logging
+# Again we could use our local image but this is fine
+docker run -d --expose 5672 -p 15672:15672 -p 5672:5672 -p 8081:8080 --name rabbitmq descartesresearch/teastore-kieker-rabbitmq
+
 # Now build the teastore all image
 docker build -t teastore-all ./utilities/tools.descartes.teastore.docker.all/
 echo "TeaStore monolithic built..."
 
 # Now run it
-docker run -e "DB_HOST=teastore-db" -p 8080:8080 -d --link teastore-db:teastore-db --name teastore-all teastore-all
+docker run -e "DB_HOST=teastore-db" -e "RABBITMQ_HOST=rabbitmq" -p 8080:8080 -d --link rabbitmq:rabbitmq --link teastore-db:teastore-db --name teastore-all teastore-all
 echo "TeaStore running on localhost..."
 echo "http://localhost:8080/tools.descartes.teastore.webui/"
